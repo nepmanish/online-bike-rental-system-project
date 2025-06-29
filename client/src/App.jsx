b6962415300962246e514329feef3b866ae23197
nepmanish <amiwho712@gmail.com>
2025-06-30 01:21:20 +0545

    add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,15 @@ import Bikes from './pages/Bikes'
 import MyBookings from './pages/MyBookings'
 import { useLocation } from 'react-router-dom'
 import Footer from './components/Footer'
+import Login from './components/Login'
 const App = () => {
 
   const [showLogin, setShowLogin] = React.useState(false)
   const isOwnerPath = useLocation().pathname.startsWith("/owner")
   return (
     <>
+    {showLogin && <Login setShowLogin={setShowLogin}/>}
+    
      {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
      <Routes>
