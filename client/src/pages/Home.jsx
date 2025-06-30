e17c3bd805464d3b78009052510cd035922d895c
nepmanish <amiwho712@gmail.com>
2025-06-30 23:27:22 +0545

    edit login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
-import Footer from '../components/Footer'
 
 
 
